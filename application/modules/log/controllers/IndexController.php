41df1093943cd9d8c515699087a01ebbc49b33a3
Nicky De Maeyer <nicky@nickubu.(none)>
2010-10-21 17:25:57 +0200

    added DbTable test in test branch

--- application/modules/log/controllers/IndexController.php
+++ application/modules/log/controllers/IndexController.php
@@ -7,8 +7,8 @@ class Log_IndexController extends Zend_Controller_Action
 	public function indexAction()
 	{
 	    $test = new Log\Model\Test();
-        //$testDbTable = new Log\Model\DbTable\Test();
+        $testDbTable = new Log\Model\DbTable\Test();
 	    var_dump($test);
-	    //var_dump($testDbTable);
+	    var_dump($testDbTable);
 	}
 }
