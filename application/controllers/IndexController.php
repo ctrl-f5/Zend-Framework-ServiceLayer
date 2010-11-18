41c43378fee168c83f32f760ff1aa3006dc6ffa8
Nicky De Maeyer <nicky@nickubu.(none)>
2010-11-18 14:07:36 +0100

    refactoring

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -13,9 +13,9 @@ class IndexController extends \Ctrl\Controller\Action
 
             $this->view->service1 = Log_Service_Test::factory(array('test' => 'my first service'));
 
-            $this->view->service2 = $this->_services->getService(Log_Service_Test::getType(), array('optionsTest' => 'viaBroker!'));
+            $this->view->service2 = $this->_services->getService(Log_Service_Test::getType(), array('optionsTest' => 'viaBroker! > these options will be set'));
 
-            $this->view->service3 = $this->_services->getService(Log_Service_Test::getType(), array('second' => 'viaBroker!'));
+            $this->view->service3 = $this->_services->getService(Log_Service_Test::getType(), array('second' => 'viaBroker! > these options will be ignored'));
 
             $this->view->service4 = $this->_services->getLocalService(Log_Service_Test::getType(), array('third' => 'viaBroker! --local'));
 
